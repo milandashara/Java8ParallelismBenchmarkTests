60d75308af16b3ac63fd64b41618aa87ebfe4aa2
milan <milandashara@gmail.com>
2014-03-22 23:08:14 +0800

    sh + results

--- Java8ParallelismBenchmarkTests/compile.sh
+++ Java8ParallelismBenchmarkTests/compile.sh
@@ -1,1 +1,1 @@
-javac -d bin -cp .;lib/poi-3.10-FINAL-20140208.jar src/*.java
+javac  src/*.java -d bin -cp "lib/poi-3.10-FINAL-20140208.jar:."
--- Java8ParallelismBenchmarkTests/run.sh
+++ Java8ParallelismBenchmarkTests/run.sh
@@ -1,1 +1,1 @@
-java -cp .;bin;lib/poi-3.10-FINAL-20140208.jar Main
+java -cp ".:bin:lib/poi-3.10-FINAL-20140208.jar" Main
